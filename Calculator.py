931ba922f92d1554d48e3825e5796a202257fda2
Throwawayaccount21212311 <81859268+Throwawayaccount21212311@users.noreply.github.com>
2021-04-10 18:51:32 +0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,17 @@
 import math
 
 
-def findupgrades(multiplier, number, price):
+def findupgrades(multiplier, number, nextprice):
     cost = 0
     upgrades = -1
-    if price == 0:
+    if nextprice == 0:
         return 100
-    while cost < price:
+    while cost < nextprice:
         upgrades = upgrades + 1
         cost = math.floor(multiplier * number ** upgrades)
         if upgrades == 100:
             return upgrades
-        if cost == price:
+        if cost == nextprice:
             return upgrades
 
 
@@ -25,11 +25,11 @@ prodperc = findupgrades(8, 1.15, int(input('How much does the next production bo
 extrspeed = findupgrades(15, 1.17, int(input('How much does the next extraction duration upgrade cost? ')))
 speedspeed = findupgrades(30, 1.22, int(input('How much does the next speed duration upgrade cost? ')))
 prodspeed = findupgrades(10, 1.15, int(input('How much does the next production duration upgrade cost? ')))
-extrbartime = round(31536000 / extrinf / (1.1 ** extrspeed), 15)
+extrbartime = round(31536000 / extrinf * (0.9 ** extrspeed), 15)
 extrbarstart = extrbartime
-speedbartime = round(31536000 / speedinf / (1.1 ** speedspeed), 15)
-prodbartime = round(31536000 / prodinf / (1.1 ** prodspeed), 15)
-speedincrease = 1.02
+speedbartime = round(31536000 / speedinf * (0.9 ** speedspeed), 15)
+prodbartime = round(31536000 / prodinf * (0.9 ** prodspeed), 15)
+speedincrease = 1.01
 extramount = 0.01
 priorextr = extrbartime
 priorspeed = speedbartime
@@ -47,7 +47,6 @@ while charge < chargereq:
     charge = charge + extramount
     priorextr = priorextr + extrbartime
 timesecspeed = priorspeed - speedbartime
-print(timesecspeed / speedbartime)
 timesecprod = priorprod - prodbartime
 if timesecprod > timesecspeed:
     timesec = timesecprod
