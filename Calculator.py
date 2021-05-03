8b69cda93fdba6a74d4eb3c740bc5c6568ab4da0
Throwawayaccount21212311 <81859268+Throwawayaccount21212311@users.noreply.github.com>
2021-05-03 07:45:46 +0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -64,5 +64,5 @@ while charge < chargereq:
     charge = charge + extramount
     timeextr = timeextr + extrbartime
 timesec = timeextr - extrbarstart
-print('{0} day, {1} hours, {2} minutes, {3} seconds'.format(timesec // 86400, timesec % 86400 // 3600, timesec % 3600 // 60, timesec % 60))
+print('{0} days, {1} hours, {2} minutes, {3} seconds'.format(timesec // 86400, timesec % 86400 // 3600, timesec % 3600 // 60, timesec % 60))
 input('\nPress ENTER to exit: ')
